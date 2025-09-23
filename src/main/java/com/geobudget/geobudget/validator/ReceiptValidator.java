f6df9baeeccc0a3fddf59335c5462c6521bfe7c3
Юрий <angelbes777@gmail.com>
2025-09-23 11:23:06 +0300

    Update validator

--- src/main/java/com/geobudget/geobudget/validator/ReceiptValidator.java
+++ src/main/java/com/geobudget/geobudget/validator/ReceiptValidator.java
@@ -9,6 +9,13 @@ public class ReceiptValidator {
         if (qrString == null || qrString.isBlank()) {
             throw new DataValidationException("qr обязателен");
         }
+
+        String[] qrList = qrString.split("&");
+
+        if (qrList.length != 6 || qrList[0].startsWith("t=") || qrList[1].startsWith("s=") || qrList[2].startsWith("fn=") || qrList[3].startsWith("i=") || qrList[4].startsWith("fp=") || qrList[5].startsWith("n=")) {
+            throw new DataValidationException("Некорректный формат qr");
+        }
+
         // Дополнительно можно проверить формат, длину и допустимые символы
         // if (!qrString.matches("^[A-Za-z0-9:=._-]+$")) {
         //     throw new DataValidationException("Некорректный формат qr");
