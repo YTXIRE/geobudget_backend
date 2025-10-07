0c31437aa433324fa5330a4cdaa691c696a6f53f
Юрий <angelbes777@gmail.com>
2025-10-07 13:59:17 +0300

    Fix cors

--- src/main/java/com/geobudget/geobudget/config/CorsConfig.java
+++ src/main/java/com/geobudget/geobudget/config/CorsConfig.java
@@ -9,15 +9,14 @@ import java.util.List;
 
 @Configuration
 public class CorsConfig {
-
     @Bean
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration configuration = new CorsConfiguration();
-        // Разрешаем конкретный домен (нельзя * если allowCredentials=true)
+        // разрешаем Swagger UI и фронтенду обращаться к backend
         configuration.setAllowedOrigins(List.of("https://backend.xire.ru"));
         configuration.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS"));
-        configuration.setAllowedHeaders(List.of("*"));
-        configuration.setAllowCredentials(true); // нужно для куки/токенов
+        configuration.setAllowedHeaders(List.of("*")); // разрешаем все заголовки
+        configuration.setAllowCredentials(true); // включаем для авторизации
 
         UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
         source.registerCorsConfiguration("/**", configuration);
--- src/main/java/com/geobudget/geobudget/config/SwaggerConfig.java
+++ src/main/java/com/geobudget/geobudget/config/SwaggerConfig.java
@@ -6,8 +6,11 @@ import io.swagger.v3.oas.annotations.security.SecurityRequirement;
 import io.swagger.v3.oas.annotations.security.SecurityScheme;
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.servers.Server;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+
+import java.util.List;
 
 @Configuration
 @OpenAPIDefinition(
@@ -22,12 +25,16 @@ import org.springframework.context.annotation.Configuration;
         bearerFormat = "JWT"
 )
 public class SwaggerConfig {
+
     @Bean
     public OpenAPI customOpenAPI() {
         return new OpenAPI()
                 .info(new Info()
                         .title("GeoBudget API")
                         .version("1.0")
-                        .description("API документация для GeoBudget"));
+                        .description("API документация для GeoBudget"))
+                .servers(List.of(
+                        new Server().url("https://backend.xire.ru")
+                ));
     }
 }
